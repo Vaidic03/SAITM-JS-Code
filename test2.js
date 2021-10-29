43cd39525f6abddc07dd5089218cd7799bd19487
unknown <vaidicarora23057@gmail.com>
2021-10-29 20:51:45 +0530

    first commit

---
+++ test2.js
@@ -0,0 +1,24 @@
+console.log("welcome to js one program");
+
+var a = 2;
+console.log(a)
+function test(){
+    console.log("from function",a)
+    let d =2;
+}
+var c =3;
+// console.log("value of d is",d);
+if(c ==3){
+    let w=3;
+    
+}
+
+function aaa(){
+    let ddd =3;
+}
+console.log
+let w=4;
+console.log("w is",w)
+
+test();
+console.log()
